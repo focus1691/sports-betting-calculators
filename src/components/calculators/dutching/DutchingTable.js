bb7d6dc55b3208658e87be934cb358cd4d12098a
Joshua <joshua@psychotechnology.com>
2020-03-08 22:58:34 +0000

    Removed default values for dutching table

--- src/components/calculators/dutching/DutchingTable.js
+++ src/components/calculators/dutching/DutchingTable.js
@@ -11,23 +11,10 @@ const DutchingTable = () => {
 	const classes = useStyles();
 	const [state, setState] = React.useState({
 		columns: [
-			{ title: "Name", field: "name" },
-			{ title: "Surname", field: "surname" },
-			{ title: "Birth Year", field: "birthYear", type: "numeric" },
-			{
-				title: "Birth Place",
-				field: "birthCity",
-				lookup: { 34: "İstanbul", 63: "Şanlıurfa" }
-			}
+			{ title: "Name", field: "name" }
 		],
 		data: [
-			{ name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 },
-			{
-				name: "Zerya Betül",
-				surname: "Baran",
-				birthYear: 2017,
-				birthCity: 34
-			}
+			{ name: "" },
 		]
 	});
 
