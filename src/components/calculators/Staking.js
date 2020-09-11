4c92dfedb149279745844bbf9cb0244c6c7cfd28
Joshua <mrjoshuaali@yahoo.co.uk>
2020-09-11 20:41:29 +0100

    Removed unused typography

--- src/components/calculators/Staking.js
+++ src/components/calculators/Staking.js
@@ -3,7 +3,6 @@ import { makeStyles } from "@material-ui/core/styles";
 import {Grid, Box} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
-import Typography from "@material-ui/core/Typography";
 import Select from "@material-ui/core/Select";
 import Chip from "@material-ui/core/Chip";
 import validator from "validator";
